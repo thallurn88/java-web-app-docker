8baec111b1b34e37d60ad534c8cbf70b84e78aa1
thallurn88 <104491945+thallurn88@users.noreply.github.com>
2022-11-04 18:12:25 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello SANVIKA Thalluru";
+			return "Hello SANVIKA THALLURU";
 		}else{
 			return "Hello " + name;
 		}
